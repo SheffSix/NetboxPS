d4cc6da1287853bb2df6fae6f9173ecfcd9df7e1
sheffsix <jim@beck81.co.uk>
2023-03-15 16:23:48 +0000

    Added Remove-NetboxDCIMCable function

---
+++ Functions/DCIM/Cables/Remove-NetboxDCIMCable.ps1
@@ -0,0 +1,35 @@
+﻿function Remove-NetboxDCIMCable {
+
+    [CmdletBinding(ConfirmImpact = 'High',
+        SupportsShouldProcess = $true)]
+    param
+    (
+        [Parameter(Mandatory = $true,
+            ValueFromPipelineByPropertyName = $true)]
+        [uint16[]]$Id,
+
+        [switch]$Force
+    )
+
+    begin {
+
+    }
+
+    process {
+        foreach ($CableID in $Id) {
+            $CurrentCable = Get-NetboxDCIMCable -Id $CableID -ErrorAction Stop
+
+            if ($Force -or $pscmdlet.ShouldProcess("Name: $($CurrentCable.Name) | ID: $($CurrentCable.Id)", "Remove")) {
+                $Segments = [System.Collections.ArrayList]::new(@('dcim', 'cables', $CurrentCable.Id))
+
+                $URI = BuildNewURI -Segments $Segments
+
+                InvokeNetboxRequest -URI $URI -Method DELETE
+            }
+        }
+    }
+
+    end {
+
+    }
+}
